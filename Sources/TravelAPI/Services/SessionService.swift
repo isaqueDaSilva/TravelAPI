2fa86642a8ed3cf12941d1e598cba9180990923c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-12 22:08:42 -0300

    feat - creating a service to handles with session tasks.

--- Sources/TravelAPI/Services/SessionService.swift
+++ Sources/TravelAPI/Services/SessionService.swift
@@ -5,4 +5,22 @@
 //  Created by Isaque da Silva on 11/11/25.
 //
 
-import Foundation
+import Fluent
+import FluentPostgresDriver
+
+enum SessionService {    
+    static func createSession(
+        with userID: UUID,
+        atVerificationCode: String,
+        refreshTokenID: String,
+        refreshToken: String,
+        using connection: any Database
+    ) async throws {
+        let query = SQLQueryString(stringInterpolation: """
+            INSERT INTO \(ident: Session.schema) (\(idents: Session.Column.createNewInstanceRows, joinedBy: ", "))
+            VALUES (\(bind: userID), \(bind: atVerificationCode), \(bind: refreshTokenID), \(bind: refreshToken))
+        """)
+        
+        try await (connection as! (any SQLDatabase)).raw(query).run()
+    }
+}
