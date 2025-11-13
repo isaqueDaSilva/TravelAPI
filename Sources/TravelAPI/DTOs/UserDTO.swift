4332b0ea2e0b3af5968e5f0baf3e6b6ee467200b
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-12 22:05:19 -0300

    feat - creating a series of DTO model for user

--- Sources/TravelAPI/DTOs/UserDTO.swift
+++ Sources/TravelAPI/DTOs/UserDTO.swift
@@ -5,4 +5,46 @@
 //  Created by Isaque da Silva on 11/11/25.
 //
 
-import Foundation
+import Vapor
+
+typealias CreateUserDTO = User.CreateUser
+typealias GetUserDTO = User.GetUser
+typealias FullUserDTO = User.FullUser
+
+extension User {
+    struct CreateUser: Content, Sendable {
+        let name: String
+        let email: String
+        let password: String
+        
+        enum CodingKeys: String, CodingKey {
+            case name
+            case email
+            case password
+            
+            var key: ValidationKey {
+                .init(stringLiteral: self.rawValue)
+            }
+        }
+    }
+    
+    struct GetUser: Content, Sendable, Authenticatable {
+        let id: UUID
+        let name: String
+        let email: String
+        let createdAt: Date
+    }
+    
+    struct FullUser: Content, Sendable {
+        let id: UUID
+        let name: String
+        let email: String
+        let passwordHash: String
+        let hasSession: Bool
+        let createdAt: Date
+        
+        func toGetUser() -> GetUser {
+            .init(id: id, name: name, email: email, createdAt: createdAt)
+        }
+    }
+}
