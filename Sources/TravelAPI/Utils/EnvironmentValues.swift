549d48e1e0d3bbf2da4792c181e64508059e9dc4
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-12 22:10:26 -0300

    feat - adding a wrapper to get enviroment variables.

--- Sources/TravelAPI/Utils/EnvironmentValues.swift
+++ Sources/TravelAPI/Utils/EnvironmentValues.swift
@@ -1,3 +1,10 @@
+//
+//  EnvironmentValues.swift
+//  TravelAPI
+//
+//  Created by Isaque da Silva on 11/11/25.
+//
+
 import Vapor
 
 enum EnvironmentValues {
@@ -11,27 +18,10 @@ enum EnvironmentValues {
     
     static func jwtIssuer() throws -> String {
         guard let subjectClaim = Environment.get("JWT_ISSUER") else {
-            print("JWT_ISSUER was not found.")
             throw Abort(.internalServerError)
         }
         
         return subjectClaim
-    }
-    
-    static func fullAccessJWTAudience() throws -> String {
-        guard let jwtSecret = Environment.get("FULL_ACCESS_JWT_AUDIENCE") else {
-            throw Abort(.internalServerError)
-        }
-        
-        return jwtSecret
-    }
-    
-    static func refreshJWTAudience() throws -> String {
-        guard let jwtSecret = Environment.get("REFRESH_JWT_AUDIENCE") else {
-            throw Abort(.internalServerError)
-        }
-        
-        return jwtSecret
     }
     
     static func accessTokenSecret() throws -> String {
@@ -48,46 +38,5 @@ enum EnvironmentValues {
         }
         
         return jwtSecret
-    }
-    
-    static func jwtSecret() throws -> String {
-        guard let jwtSecret = Environment.get("JWT_SECRET") else {
-            throw Abort(.internalServerError)
-        }
-        
-        return jwtSecret
-    }
-    
-    static func cupcakeCornerJWTSUB() throws -> String {
-        guard let subjectClaim = Environment.get("CUPCAKE_CORNER_JWTSUB") else {
-            print("CUPCAKE_CORNER_JWTSUB was not found.")
-            throw Abort(.internalServerError)
-        }
-        
-        return subjectClaim
-    }
-    
-    static func tlsCertificatePath() throws -> String {
-        guard let tlsPEMCertificatePath = Environment.get("TLS_CERTIFICATE_PATH") else {
-            throw Abort(.internalServerError)
-        }
-        
-        return tlsPEMCertificatePath
-    }
-    
-    static func tlsPrivateKey() throws -> String {
-        guard let tlsPrivateKey = Environment.get("TLS_KEY_PATH") else {
-            throw Abort(.internalServerError)
-        }
-        
-        return tlsPrivateKey
-    }
-    
-    static func productImagePath(with imageName: String) throws -> String {
-        guard let cupcakeImageFolder = Environment.get("PRODUCT_IMAGE_FOLDER") else {
-            throw Abort(.internalServerError)
-        }
-        
-        return cupcakeImageFolder + "/" + imageName
     }
 }
