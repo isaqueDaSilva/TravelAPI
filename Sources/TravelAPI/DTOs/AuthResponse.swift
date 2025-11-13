ecbc13246891b8253f454a80d55febc00b8b6857
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-12 22:03:44 -0300

    feat - creating a model to structure the sign in and sign up response

--- Sources/TravelAPI/DTOs/AuthResponse.swift
+++ Sources/TravelAPI/DTOs/AuthResponse.swift
@@ -5,4 +5,10 @@
 //  Created by Isaque da Silva on 11/12/25.
 //
 
-import Foundation
+import Vapor
+
+struct AuthResponse: Content {
+    let userProfile: GetUserDTO
+    let accessToken: String
+    let refreshTokenID: String
+}
