bac25e73aae1e2fa6fc4e916abfef445fd1ac03d
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-12 22:09:16 -0300

    feat - creating a service for handles with JWT task.

--- Sources/TravelAPI/Services/JWTService.swift
+++ Sources/TravelAPI/Services/JWTService.swift
@@ -5,4 +5,27 @@
 //  Created by Isaque da Silva on 11/11/25.
 //
 
-import Foundation
+import JWT
+import Vapor
+
+enum JWTService {
+    static func createPairOfJWTs(
+        userID: UUID,
+        jwtHandler: Request.JWT
+    ) async throws -> (accessToken: String, refreshToken: String, validationCode: String, refreshTokenID: String) {
+        let validationCode = UUID().uuidString
+        let accessTokenPayload = try Payload(with: userID, validationCode: validationCode, expirationTime: .tenMinutes)
+        let refreshTokenPayload = try Payload(with: userID, validationCode: validationCode, expirationTime: .sevenDays)
+        
+        let accessToken = try await jwtHandler.sign(accessTokenPayload)
+        let refreshToken = try await jwtHandler.sign(refreshTokenPayload)
+        
+        return (
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            validationCode: validationCode,
+            refreshTokenID: refreshTokenPayload.jwtID.value
+        )
+    }
+}
+
