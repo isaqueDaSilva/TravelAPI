3a4d959cd5b17dfd4634302125e09c0a5ab7e6ab
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-12 22:03:11 -0300

    feat - creating two routes and your handler to handle with sign in and sign up actions

--- Sources/TravelAPI/Controllers/AuthController.swift
+++ Sources/TravelAPI/Controllers/AuthController.swift
@@ -5,4 +5,71 @@
 //  Created by Isaque da Silva on 11/11/25.
 //
 
-import Foundation
+import Vapor
+
+struct AuthController: RouteCollection, ProtectedRouteProtocol {
+    func boot(routes: any RoutesBuilder) throws {
+        let authRoute = routes.grouped("auth")
+        let userProtectedRoute = userProtectedRoute(by: authRoute)
+        
+        // -> POST /auth/signup
+        authRoute.post("signup") { try await self.signup(with: $0) }
+        
+        // -> POST /auth/signin
+        authRoute.post("signin") { try await self.signin(with: $0) }
+    }
+    
+    @Sendable
+    private func signup(with request: Request) async throws -> AuthResponse {
+        try CreateUserDTO.validate(query: request)
+        
+        let createUserDTO = try request.content.decode(CreateUserDTO.self)
+        let hashedPassword = try Bcrypt.hash(createUserDTO.password)
+        
+        
+        return try await request.db.transaction { database in
+            let newUser = try await UserService.createUser(
+                with: createUserDTO,
+                hashedPassword: hashedPassword,
+                using: database
+            )
+            
+            let pairOfTokens = try await JWTService.createPairOfJWTs(userID: newUser.id, jwtHandler: request.jwt)
+            
+            try await SessionService.createSession(
+                with: newUser.id,
+                atVerificationCode: pairOfTokens.validationCode,
+                refreshTokenID: pairOfTokens.refreshTokenID,
+                refreshToken: pairOfTokens.refreshToken,
+                using: database
+            )
+            
+            return .init(
+                userProfile: newUser,
+                accessToken: pairOfTokens.accessToken,
+                refreshTokenID: pairOfTokens.refreshTokenID
+            )
+        }
+    }
+    
+    @Sendable
+    private func signin(with request: Request) async throws -> AuthResponse {
+        let user = try request.auth.require(GetUserDTO.self)
+        
+        let pairOfTokens = try await JWTService.createPairOfJWTs(userID: user.id, jwtHandler: request.jwt)
+        
+        try await SessionService.createSession(
+            with: user.id,
+            atVerificationCode: pairOfTokens.validationCode,
+            refreshTokenID: pairOfTokens.refreshTokenID,
+            refreshToken: pairOfTokens.refreshToken,
+            using: request.db
+        )
+        
+        return .init(
+            userProfile: user,
+            accessToken: pairOfTokens.accessToken,
+            refreshTokenID: pairOfTokens.refreshTokenID
+        )
+    }
+}
