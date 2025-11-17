9a3fc9d24c492f42137c8b0aa4e65a705ef90a6b
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-17 16:40:42 -0300

    cleanup - removing code that previously generate refresh token.

--- Sources/TravelAPI/Services/JWTService.swift
+++ Sources/TravelAPI/Services/JWTService.swift
@@ -9,23 +9,13 @@ import JWT
 import Vapor
 
 enum JWTService {
-    static func createPairOfJWTs(
+    static func generateJWT(
         userID: UUID,
         jwtHandler: Request.JWT
-    ) async throws -> (accessToken: String, refreshToken: String, validationCode: String, refreshTokenID: String) {
-        let validationCode = UUID().uuidString
-        let accessTokenPayload = try Payload(with: userID, validationCode: validationCode, expirationTime: .tenMinutes)
-        let refreshTokenPayload = try Payload(with: userID, validationCode: validationCode, expirationTime: .sevenDays)
+    ) async throws -> String {
+        let accessTokenPayload = try Payload(with: userID, expirationTime: .tenMinutes)
         
-        let accessToken = try await jwtHandler.sign(accessTokenPayload)
-        let refreshToken = try await jwtHandler.sign(refreshTokenPayload)
-        
-        return (
-            accessToken: accessToken,
-            refreshToken: refreshToken,
-            validationCode: validationCode,
-            refreshTokenID: refreshTokenPayload.jwtID.value
-        )
+        return try await jwtHandler.sign(accessTokenPayload)
     }
 }
 
