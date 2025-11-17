2e08ab0389a4896091de575296219c4a4f0539ca
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-17 16:32:03 -0300

    feat - creating a session id generator to serves as refresh token.

--- Sources/TravelAPI/Auth/Sessions/User+Session.swift
+++ Sources/TravelAPI/Auth/Sessions/User+Session.swift
@@ -5,4 +5,14 @@
 //  Created by Isaque da Silva on 11/17/25.
 //
 
-import Foundation
+import Vapor
+
+extension GetUserDTO {
+    func generateSessionID() -> String {
+        let currentDate = Date.now.ISO8601Format().replacingOccurrences(of: ":", with: "")
+        let randomNumber = Int.random().description
+        let id = self.id.uuidString + currentDate + randomNumber
+        
+        return id
+    }
+}
