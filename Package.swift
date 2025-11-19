ed1c986459611285dc851377539f40cfeaa1bf93
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-19 20:36:01 -0300

    feat - adding redis client package to the projetct

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "5.0.0")
+        .package(url: "https://github.com/vapor/jwt.git", from: "5.0.0"),
+        .package(url: "https://github.com/vapor/redis.git", from: "4.14.0"),
     ],
     targets: [
         .executableTarget(
@@ -26,7 +27,8 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "JWT", package: "jwt")
+                .product(name: "JWT", package: "jwt"),
+                .product(name: "Redis", package: "redis")
             ],
             swiftSettings: swiftSettings
         ),
