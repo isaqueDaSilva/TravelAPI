053e2794e8f38a821c49a01bf6f7dd08c369fd6a
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-11-21 02:51:22 -0300

    style - adding a typealias to access more easyly the DriverProfile dto model

--- Sources/TravelAPI/DTOs/DriverDTO.swift
+++ Sources/TravelAPI/DTOs/DriverDTO.swift
@@ -5,4 +5,13 @@
 //  Created by Isaque da Silva on 11/20/25.
 //
 
-import Foundation
+import Vapor
+
+typealias DriverProfile = Driver.DriverProfile
+
+extension Driver {
+    struct DriverProfile: Content {
+        let name: String?
+        let carModel: String
+    }
+}
